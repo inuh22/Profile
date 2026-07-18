f4dae5479505af3470e1d5c8e39a13cf70c3e603
inuh22 <johnjuni411@gmail.com>
2026-07-18 20:23:24 +0800

    SMTP

--- app/routes/contact.py
+++ app/routes/contact.py
@@ -1,20 +1,50 @@
 from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse
 from app.config import templates
+from app.services.mailer import contact_mailer
 from typing import Annotated
+from pydantic import EmailStr
+
 
 router = APIRouter()
 
-@router.post('/contact-form')
+
+@router.get('/mail-form')
+async def mail_form(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name='fragments/mail_form.html',
+        context={}
+    )
+
+@router.post('/send-mail-form')
 async def contact_form(
         request:Request,
         name : Annotated[str, Form()],
-        email : Annotated[str, Form()],
+        email : Annotated[EmailStr, Form()],
         message : Annotated[str, Form()]
     ):
+
     print(name)
     print(email)
     print(message)
+
+    if not name or not email or not message:
+        return templates.TemplateResponse(
+            request=request,
+            name='fragments/unexpected.html',
+            context={}
+        )
+
+    try:
+        await contact_mailer(name, email, message)
+    except Exception:
+        return templates.TemplateResponse(
+            request=request,
+            name="fragments/unexpected.html",
+            context={},
+            status_code=500
+        )
 
     return templates.TemplateResponse(
         request=request,
